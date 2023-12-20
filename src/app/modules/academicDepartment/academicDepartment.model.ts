82722ad19730b1e8e3849e92124f6bfddd6b4873
Abu <abujakaria316@gmail.com>
2023-12-21 03:52:02 +0600

    department is exist validation

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -4,14 +4,33 @@ import { TAcademicDepartment } from './academicDepartment.interface';
 const AcademicDepartmentSchema = new Schema<TAcademicDepartment>({
   name: {
     type: String,
-    required: true,
     unique: true,
+    required: true,
   },
   academicFaculty: {
     type: Schema.Types.ObjectId,
     ref: 'AcademicFaculty',
     required: true,
   },
+});
+
+AcademicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExist = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+  if (isDepartmentExist) {
+    throw new Error(`${this.name} is already exist`);
+  }
+  next();
+});
+
+AcademicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+  const isDepartmentExist = await AcademicDepartment.findOne(query);
+  if (!isDepartmentExist) {
+    throw new Error("Department doesn't exist");
+  }
+  next();
 });
 
 export const AcademicDepartment = model<TAcademicDepartment>(
