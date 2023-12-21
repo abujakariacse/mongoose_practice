6e7784b678cbee7cde7e3b6b81e4507e71f4eef2
Abu <abujakaria316@gmail.com>
2023-12-22 03:05:40 +0600

    session,transaction and rollback

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import config from '../../config';
 import { TAcademicSemester } from '../academicSemester/academicSemester.interface';
 import { AcademicSemester } from '../academicSemester/academicSemester.model';
@@ -6,6 +7,8 @@ import { Student } from '../student/student.model';
 import { TUser } from './user.interface';
 import { User } from './user.model';
 import { generateStudentId } from './user.utils';
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
 
 const createUserToDB = async (password: string, payload: TStudent) => {
   // Create a user obeject to define user properties
@@ -16,30 +19,45 @@ const createUserToDB = async (password: string, payload: TStudent) => {
     payload.admissionSemester,
   );
 
-  userData.id = await generateStudentId(admissionSemester as TAcademicSemester);
+  const session = await mongoose.startSession();
 
-  // set user role
-  userData.role = 'student';
+  try {
+    session.startTransaction();
 
-  // set user password
-  userData.password = password || (config.default_password as string);
+    userData.id = await generateStudentId(
+      admissionSemester as TAcademicSemester,
+    );
 
-  // Built in static method
-  const newUser = await User.create(userData);
+    // set user role
+    userData.role = 'student';
 
-  if (Object.keys(newUser).length) {
-    payload.id = newUser.id;
-    payload.user = newUser._id;
+    // set user password
+    userData.password = password || (config.default_password as string);
 
-    const newStudent = (
-      await (await Student.create(payload)).populate('admissionSemester')
-    ).populate({
-      path: 'academicDepartment',
-      populate: {
-        path: 'academicFaculty',
-      },
-    });
+    // create a user (Transaction - 1)
+    // Built in static method
+    const newUser = await User.create([userData], { session });
+
+    if (!newUser.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'User creation failed');
+    }
+
+    payload.id = newUser[0].id;
+    payload.user = newUser[0]._id;
+
+    // create student (transaction - 2)
+    const newStudent = await Student.create([payload], { session });
+
+    if (!newStudent.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Student creation failed');
+    }
+    await session.commitTransaction();
+    await session.endSession();
+
     return newStudent;
+  } catch (err) {
+    await session.abortTransaction();
+    await session.endSession();
   }
 };
 
